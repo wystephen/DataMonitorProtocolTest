8dcf64755418a239fa3ad1fe492f43dbda117ac0
wang stephen <551619855@qq.com>
2017-01-08 22:14:46 +0800

    Initial commit

---
+++ DataMonitorProtocol/BaseHandle.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# carete by steve at  2017 / 01 / 08　22:10
+
+
+class BaseHandle:
+    '''
+    initial the handle.
+    '''
+
+    def __init__(self):
+        # Search Masters
+
+        # Connect to Masters
+
+        # Creat Data Send thread(not individual thread)
+
+
+    def search_master(self,possible_device):
+        
+
+
