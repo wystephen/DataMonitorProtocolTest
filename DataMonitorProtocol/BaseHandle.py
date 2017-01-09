7bc0bccf085742d0488301b5542fc2c7ec8e6b32
wang stephen <551619855@qq.com>
2017-01-09 12:40:14 +0800

    tcp test server ok.

--- DataMonitorProtocol/BaseHandle.py
+++ DataMonitorProtocol/BaseHandle.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 # carete by steve at  2017 / 01 / 08　22:10
+import socket
 
+import time
 
 class BaseHandle:
     '''
@@ -8,7 +10,12 @@ class BaseHandle:
     '''
 
     def __init__(self):
+        self.just_for_test_connect()
+
+
+
         # Search Masters
+
 
         # Connect to Masters
 
@@ -17,6 +24,26 @@ class BaseHandle:
 
     def search_master(self,possible_device):
 
+        return 0
 
+
+    def just_for_test_connect(self):
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect(('127.0.0.1',1994))
+
+        index = 0
+        while True:
+            s.send(b'is ok')
+            index += 1
+            time.sleep(1)
+            if(index > 20):
+                break
+        s.send(b'exit')
+        s.close()
+
+
+
+if __name__ == '__main__':
+    tmp = BaseHandle()
 
 
--- TestVirtualClass/VirtualHost.py
+++ TestVirtualClass/VirtualHost.py
@@ -10,5 +10,30 @@ class TestSimpleHost:
         self.s_handle = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
         self.s_handle.bind(('127.0.0.1',1994))
+        self.s_handle.listen(10)
 
-    def 
+        while True:
+            sock, addr = self.s_handle.accept()
+
+            t = threading.Thread(target=self.VirtualBuildConnect,args=(sock,addr))
+            t.start()
+
+
+    def VirtualBuildConnect(self,sock,addr):
+        print("Accept new connect from {0}".format(addr))
+
+        sock.send(b'Welcom')
+
+        while True:
+            data = sock.recv(1024)
+
+            if data == 'close' :
+                break
+            else:
+                print("data is : ")
+
+        sock.close()
+        print("Connection from {1} closed.".format(addr))
+
+if __name__ == '__main__':
+    tmp = TestSimpleHost()
