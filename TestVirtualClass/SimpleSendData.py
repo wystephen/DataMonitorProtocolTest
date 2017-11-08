68ca1bac0b47462c1497ec6f3ce83e1297123022
wang stephen <551619855@qq.com>
2017-11-08 15:02:21 +0800

    ~

--- TestVirtualClass/SimpleSendData.py
+++ TestVirtualClass/SimpleSendData.py
@@ -1,8 +1,16 @@
 # -*- coding:utf-8 -*-
 # carete by steve at  2017 / 01 / 25　17:19
 
-
+import time
 import socket
 
 if __name__ == '__main__':
-    sk = socket.socket()
+    sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    address = ('127.0.0.1',9999)
+    sk.connect(address)
+
+    for i in range(100000):
+        tmp_info = ('<Name:fast> {0} {1} {2}'.format(i,i*10.0,i*i))
+
+        sk.send(tmp_info.encode('utf-8'))
+        time.sleep(0.5)
